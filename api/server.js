f05964e513fd0c3f58ccd42e1a4eeae01d088311
Jeffrey <j.lo128456@gmail.com>
2025-02-20 22:34:54 +1000

    updated server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'data', 'data.json'));
+const router = jsonServer.router(path.join(__dirname, '..', 'data', 'data.json'));
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
 
