e1b3cdff1532367be77646464ed92cb0ef87c8a6
Jeffrey <j.lo128456@gmail.com>
2025-02-20 21:15:33 +1000

    added server.js and upodate code

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+// Enable CORS for all origins (or specify a particular origin)
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running on http://localhost:3000');
+});
