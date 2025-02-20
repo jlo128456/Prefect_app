64fec8c0ac179c26a991248ab39e46bedad6b93a
Jeffrey <j.lo128456@gmail.com>
2025-02-20 22:28:55 +1000

    updated vercel.json

---
+++ api/server.js
@@ -0,0 +1,17 @@
+// server.js
+const jsonServer = require('json-server');
+const path = require('path');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'data', 'data.json'));
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+// Enable CORS
+server.use(cors());
+
+// Use JSON Server's default middlewares and router
+server.use(middlewares);
+server.use(router);
+
+// Export (no `listen()` call here)
+module.exports = server;
