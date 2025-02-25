7609a83d482f212a31d90d22e85eaf3bb45bcfa3
Jeffrey <j.lo128456@gmail.com>
2025-02-25 19:38:57 +1000

    updated utlis.js

--- api/public/js/utils.js
+++ api/public/js/utils.js
@@ -22,13 +22,13 @@ export function formatDateTime(dateString) {
   
   export function applyStatusColor(statusElement, status) {
     if (status === "Pending") {
-      statusElement.style.backgroundColor = "red";
+      statusElement.style.backgroundColor = "Green";
       statusElement.style.color = "white";
     } else if (status === "In Progress") {
       statusElement.style.backgroundColor = "yellow";
       statusElement.style.color = "black";
     } else if (status === "Completed") {
-      statusElement.style.backgroundColor = "green";
+      statusElement.style.backgroundColor = "Red";
       statusElement.style.color = "white";
     } else if (status === "Completed - Pending Approval") {
       statusElement.style.backgroundColor = "orange";
