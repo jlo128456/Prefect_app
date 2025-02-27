e02293a2a02aa6051912f78411df00f56c4a9ef7
Jeffrey <j.lo128456@gmail.com>
2025-02-27 23:36:46 +1000

    updated js

--- api/public/js/api.js
+++ api/public/js/api.js
@@ -1,6 +1,6 @@
 import { G } from './globals.js';
 import { populateAdminJobs, populateContractorJobs, showDashboard } from './dashboard.js';
-import { loadData } from './api.js';
+
 
 const API_BASE_URL = 'https://prefect-app.onrender.com';
 
