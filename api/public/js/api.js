c7f975e10e24a19fc9b6347b2c7d8dadbe89053c
Jeffrey <j.lo128456@gmail.com>
2025-03-11 21:33:30 +1000

    Update api (11/03/2025)9.20pm

--- api/public/js/api.js
+++ api/public/js/api.js
@@ -45,7 +45,7 @@ export async function updateJobStatus(id) {
     const job = await response.json();
     if (!job) return;
 
-    const currentTime = new Date().toLocaleString();
+    const currentTime = new Date().toISOString();
     let updatedStatus, contractorStatus, statusMessage;
 
     switch (job.status) {
